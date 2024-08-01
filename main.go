521e6c065bf9c5a7e18b3627226b9b719ad192cc
Ye <murtazobroimshoevm4@gmail.com>
2024-08-01 07:34:25 -0700

    first commit

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	
+	r := gin.Default()
+	r.GET("/data/list",ForGet)
+	r.POST("/data/add",ForPost)
+}
+func ForPost(c *gin.Context)  {
+	fmt.Println("helo")
+	c.JSON(200,"succes")
+}
+func ForGet(c *gin.Context)  {
+	fmt.Println("privet")
+	c.JSON(200,"succes2")
 }
